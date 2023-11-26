780c171c06d21f61789498f120698a6d18944f45
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2023-11-26 12:44:39 +0545

    Initial Commit

---
+++ DataFetchAndPost/Model/UserDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFetchAndPost.Model
+{
+    public class UserDetails
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Name {  get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int? DepartmentId { get; set; }
+    }
+}
---
+++ DataFetchAndPost/Program.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using DataFetchAndPost.Model;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public class Program
+{
+    static void Main()
+    {
+        string sourceConnectionString = ConfigurationManager.ConnectionStrings["FetchConnection"].ConnectionString;
+        string destinationConnectionString = ConfigurationManager.ConnectionStrings["PostConnection"].ConnectionString;
+
+        try
+        {
+            using (var sourceConnection = new SqlConnection(sourceConnectionString))
+            {
+                sourceConnection.Open();
+
+                var data = sourceConnection.Query<UserDetails>("select distinct a.UserName,a.email as Email,b.email, b.attendeesName as Name, b.Phone, b.Address,b.DepartmentId,c.DepartmentName\r\nfrom aspnetusers a\r\nleft join Attendees_Setup b on a.email = b.email\r\nleft join Department_Setup c on c.DepartmentId = b.DepartmentId\r\nOrder by DepartmentId").ToList();
+
+                using (var destinationConnection = new SqlConnection(destinationConnectionString))
+                {
+                    destinationConnection.Open();
+                    foreach( var item in data )
+                    {
+                        string insertUserQuery = "INSERT INTO Auth.tblUser (UserName, Password,Name, Address,ContactNo,Email,Status, CreatedDate,DepartmentId,RoleId)\r\nValues(@UserName,'Dishhome@123',@Name,@Address,@Phone,@Email,1,GetDate(),@DepartmentId, 4)";
+                        destinationConnection.Execute(insertUserQuery, item);
+                    }
+                }
+            }
+        }
+        catch (SqlException ex) 
+        {
+            Console.WriteLine($"Error accessing the database: {ex.Message}");
+        }
+    }
+
+}
